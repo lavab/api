6e121617d18b936c3e320fe546b057807f5202da
makkalot <makkalot@gmail.com>
2014-11-08 20:11:11 +0200

    custom errors for database related problems

--- db/errors.go
+++ db/errors.go
@@ -1,5 +1,39 @@
 package db
 
+import (
+	"fmt"
+)
+
+type DbError struct {
+	err   error
+	msg   string
+	table RethinkTable
+}
+
+func (derr *DbError) Error() string {
+	return fmt.Sprintf(
+		"%s - Db: %s - Table : %s - %s ",
+		derr.msg,
+		derr.table.GetDbName(),
+		derr.table.GetTableName(),
+		derr.err)
+}
+
+func NewDbErr(t RethinkTable, err error) *DbError {
+	return &DbError{
+		err:   err,
+		table: t,
+	}
+}
+
+func NewDbErrWithMsg(t RethinkTable, err error, msg string) *DbError {
+	return &DbError{
+		err:   err,
+		table: t,
+		msg:   msg,
+	}
+}
+
 type ConnectionError struct {
 	error
 	WrongAuthKey bool
