bff6305cf8e94248b1e655f4a3a0531c7753a9c5
Piotr Zduniak <piotr@zduniak.net>
2015-01-15 14:55:49 +0100

    Public key updating

--- routes/accounts.go
+++ routes/accounts.go
@@ -429,6 +429,7 @@ type AccountsUpdateRequest struct {
 	FactorValue     []string    `json:"factor_value" schema:"factor_value"`
 	Token           string      `json:"token" schema:"token"`
 	Settings        interface{} `json:"settings" schema:"settings"`
+	PublicKey       string      `json:"public_key" schema:"public_key"`
 }
 
 // AccountsUpdateResponse contains the result of the AccountsUpdate request.
@@ -563,6 +564,38 @@ func AccountsUpdate(c web.C, w http.ResponseWriter, r *http.Request) {
 
 	if input.Settings != nil {
 		user.Settings = input.Settings
+	}
+
+	if input.PublicKey !+ "" {
+		key, err:= env.Keys.FindByFingerprint(input.PublicKey)
+		if err != nil {
+			env.Log.WithFields(logrus.Fields{
+				"error": err.Error(),
+				"fingerprint": input.PublicKey,
+			}).Error("Unable to find a key")
+
+			utils.JSONResponse(w, 400, &AccountsUpdateResponse{
+				Success: false,
+				Message: "Invalid public key",
+			})
+			return
+		}
+
+		if key.Owner != user.ID {
+			env.Log.WithFields(logrus.Fields{
+				"user_id:": user.ID,
+				"owner": key.Owner,
+				"fingerprint": input.PublicKey,
+			}).Error("Unable to find a key")
+
+			utils.JSONResponse(w, 400, &AccountsUpdateResponse{
+				Success: false,
+				Message: "Invalid public key",
+			})
+			return			
+		}
+
+		user.PGPKey = input.PublicKey
 	}
 
 	if input.FactorType != "" {
