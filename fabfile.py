ddeeed1f4f30f68074c378938148614e0556a1db
Piotr Zduniak <piotr@zduniak.net>
2015-02-01 21:51:25 +0100

    New deployment flow

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+import os, random, string
+from fabric.api import run, env
+
+def deploy():
+	branch = os.getenv('DRONE_BRANCH', 'master')
+	commit = os.getenv('DRONE_COMMIT', 'master')
+	tmp_dir = '/tmp/' + ''.join(random.choice(string.lowercase) for i in xrange(10))
+
+	with cd(tmp_dir):
+		run('git clone git@github.com:lavab/api.git')
+		with cd('api'):
+			run('docker build -t registry.lavaboom.io/lavaboom/api-' + branch + ' .')
+			
+		run('git clone git@github.com:lavab/docker.git')
+		with cd('docker/runners'):
+			run('docker rm -f api-' + branch)
+			run('./api-' + branch + '.sh')
+
+	run('rm -r ' + tmp_dir)
