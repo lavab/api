bb46f03e0aadc7822787f8219c82084f98d4d946
Piotr Zduniak <piotr@zduniak.net>
2015-02-20 11:15:44 +0100

    Added manifests to emails.List

--- db/table_emails.go
+++ db/table_emails.go
@@ -96,6 +96,15 @@ func (e *EmailsTable) List(
 		term = term.Slice(offset, offset+limit)
 	}
 
+	// Add manifests
+	term = term.InnerJoin(gorethink.Table("emails").Pluck("id", "manifest"), func(thread gorethink.Term, email gorethink.Term) gorethink.Term {
+		return thread.Field("emails").Contains(email.Field("id"))
+	}).Without(map[string]interface{}{
+		"right": map[string]interface{}{
+			"id": true,
+		},
+	}).Zip()
+
 	// Run the query
 	cursor, err := term.Run(e.GetSession())
 	if err != nil {
