9c5c4357ae37cf0ddf142467870680461cafe044
Andrei Simionescu <ansimionescu@gmail.com>
2015-08-12 13:38:58 +0200

    Temporary fix to support old file format

--- models/file.go
+++ models/file.go
@@ -2,9 +2,10 @@ package models
 
 // File is an encrypted file stored by Lavaboom
 type File struct {
+	Encrypted
 	Resource
 
-	Meta interface{} `json:"meta" gorethink:"meta"`
-	Body []byte      `json:"body" gorethink:"body"`
-	Tags []string    `json:"tags" gorethink:"tags"`
+	Meta interface{} `json:"meta" gorethink:"meta,omitempty"`
+	Body []byte      `json:"body" gorethink:"body,omitempty"`
+	Tags []string    `json:"tags" gorethink:"tags,omitempty"`
 }
